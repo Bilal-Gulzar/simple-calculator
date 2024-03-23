cfe7ad94d65dbe538fc76816f376b034243924fc
Bilal-GUlzar <bilalgul415@gmail.com>
2024-03-23 18:25:45 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+// Simple Calculator 
+
+ import inquirer from "inquirer";
+import NumberPrompt from "inquirer/lib/prompts/number.js";
+
+ const answer = await inquirer.prompt([{message:"Enter first Number", type:"number", name:"firstNumber"},
+{message:"Enter second Number", type:"number", name:"SecondNumber"},
+{message:"Choose one of the operators to perform operation", type:"list", name:"operator", choices:["Addition", "Subtraction", "Multiplication", "Division"]}
+]);
+  
+
+//condition statment
+
+if (answer.operator === "Addition"){
+  console.log(answer.firstNumber + answer.SecondNumber);
+} 
+
+else if (answer.operator === "Subtraction"){
+  console.log(answer.firstNumber - answer.SecondNumber);
+}
+
+else if(answer.operator === "Multiplication"){
+    console.log(answer.firstNumber * answer.SecondNumber);
+}
+
+else if(answer.operator === "Division"){
+    console.log(answer.firstNumber / answer.SecondNumber);
+}
+
+else{
+
+    console.log("please choose valid operaor");
+}
